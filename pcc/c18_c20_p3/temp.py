0b7c0590db60c67ebc666c397936041133b5c7ee
cy <35268618+lowkvel@users.noreply.github.com>
2020-11-30 14:27:38 +0800

    django installment

--- pcc/c18_c20_p3/temp.py
+++ pcc/c18_c20_p3/temp.py
@@ -1,6 +1,6 @@
 # temp
 
-# spec
+# spec for the project
 """
 We’ll write a web app called Learning Log 
 that allows users to log the topics they’re interested in 
@@ -12,4 +12,14 @@ and invite users to either register or log in.
 Once logged in, a user can create new topics, 
 add new entries, and read and edit existing entries.
 """
+
+# use a new venv
+"""
+Create a new directory for the project
+CD into the directory created above
+Run the command "python -m venv new_env_name" to create a new venv
+Run the command "source new_env_name/bin/activate" to activate the new venv
+Run the command "deactivate" to deactivate the new venv
+"""
+
 
