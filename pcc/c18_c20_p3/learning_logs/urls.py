e153dd115ab2d103a8413b5a91cd177c27e9caf7
cy <35268618+lowkvel@users.noreply.github.com>
2020-11-30 18:24:26 +0800

    extras

--- pcc/c18_c20_p3/learning_logs/urls.py
+++ pcc/c18_c20_p3/learning_logs/urls.py
@@ -6,6 +6,8 @@ from . import views
 app_name = 'learning_logs'
 
 urlpatterns = [
-    # Home page
-    path('', views.index, name='index'),    # url pattern, index() functions in views.py, name of this pattern
+    # url pattern, index() functions in views.py, name of this pattern
+    path('', views.index, name='index'),    # home page
+    path('topics/', views.topics, name='topics'),   # pages that shows all topics
+    path('topics/<int:topic_id>/', views.topic, name='topic'),  # detail of a single topic
 ]
--- pcc/c18_c20_p3/learning_logs/views.py
+++ pcc/c18_c20_p3/learning_logs/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
 
+from .models import Topic
+
 # Create your views here.
 def index(request):
     # the home page for learning log
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    # show all topics
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    """
+    A context is a dictionary in which 
+    the keys are names we’ll use in the template to access the data, and 
+    the values are the data we need to send to the template.
+    """
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    # show a single topic and all its entries
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')   # in reverse order
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
+
 
