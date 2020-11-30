c48a538a6e66df19bc50f9ee96bdfca69e18742e
cy <35268618+lowkvel@users.noreply.github.com>
2020-11-30 14:50:55 +0800

    create django project and database for it

--- pcc/c18_c20_p3/temp.py
+++ pcc/c18_c20_p3/temp.py
@@ -13,13 +13,33 @@ Once logged in, a user can create new topics,
 add new entries, and read and edit existing entries.
 """
 
-# use a new venv
+# create and use a new venv
 """
-Create a new directory for the project
-CD into the directory created above
-Run the command "python -m venv new_env_name" to create a new venv
-Run the command "source new_env_name/bin/activate" to activate the new venv
-Run the command "deactivate" to deactivate the new venv
+Create a new directory for the project named new_dir
+CD into the directory new_dir created above
+Run the command "python -m venv new_venv_name" to create a new venv
+Run the command "source new_venv_name/bin/activate" to activate the new venv
+Run the command "deactivate" to deactivate the new venv if needed
+* the new venv folder "new_venv_name" will be located under new_dir
 """
 
+# install Django for the new venv
+"""
+Run the command "pip install django" to install Django on current venv
+* the Django packages will be located under new_dir/new_venv_name/...
+"""
 
+# create a Django project under the new venv
+"""
+Run the command "django-admin startproject project_name ." to create a Django project
+* the project folders "project_name" will be located under new_dir
+
+The settings.py file controls how Django interacts with your system and manages your project. 
+The urls.py file tells Django which pages to build in response to browser requests. 
+The wsgi.py file helps Django serve the files it creates. The filename is an acronym for "web server gateway interface".
+"""
+
+# create the database for the project
+"""
+Run the command "python manage.py migrate" to create the database
+"""
