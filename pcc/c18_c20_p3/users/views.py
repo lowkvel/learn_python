98e935e9f186813f673ad36c6011617c9f30c2c5
cy <35268618+lowkvel@users.noreply.github.com>
2020-12-03 11:12:11 +0800

    user
    register

--- pcc/c18_c20_p3/users/views.py
+++ pcc/c18_c20_p3/users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def register(request):
+    if request.method != 'POST':    # Display blank registration form if we are not responding to a POST request, 
+                                    # that means it is a initial GET request used for retrive the initial blank registgration form
+        form = UserCreationForm()
+    else:                           # Process completed form if POST, the data is located in request.POST
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():             # if valid
+            new_user = form.save()      # create the user, save the username and the hash of the password into the database
+            login(request, new_user)    # log the user in after sucessful registration
+            return redirect('learning_logs:index')  # redirect to the home page
+    # display a blank or invalid form
+    context = {'form': form}
+    return render(request, 'registration/register.html', context)
