0b1e0afac087743a21b020b369e3ceb01d0ee24c
cy <35268618+lowkvel@users.noreply.github.com>
2020-12-21 14:43:15 +0800

    admin forms

--- typeidea/typeidea/blog/adminforms.py
+++ typeidea/typeidea/blog/adminforms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class PostAdminForm(forms.ModelForm):
-    desc = forms.CharField(widget=forms.Textarea, label='摘要', required=False)
+    desc = forms.CharField(widget=forms.Textarea, label='摘要', required=False)     # desc field changed to Textarea type
