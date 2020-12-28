36acec94d28eaeab3499a7efb63139cca9b28e59
cy <35268618+lowkvel@users.noreply.github.com>
2020-12-28 17:10:33 +0800

    main app installed

--- typeidea/typeidea/typeidea/settings/base.py
+++ typeidea/typeidea/typeidea/settings/base.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #'typeidea',    # uncomment this to use templates under typeidea app folder, not suggested
+
     'blog',
     'config',
     'comment',
