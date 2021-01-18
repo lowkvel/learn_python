a4bd81f9a659569c35a501b6fde7f2cf8045ad11
cy <35268618+lowkvel@users.noreply.github.com>
2021-01-18 16:19:37 +0800

    redis configuration

--- typeidea/typeidea/typeidea/settings/develop.py
+++ typeidea/typeidea/typeidea/settings/develop.py
@@ -19,5 +19,21 @@ DATABASES = {
 }
 """
 
+REDIS_URL = 'redis://127.0.0.1:6379'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': REDIS_URL,
+        'TIMEOUT': 300,
+        'OPTIONS': {
+            'PASSWORD': 'redis2333',
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+            'PARSER_CLASS': 'redis.connection.HiredisParser',
+        },
+        'CONNECTION_POOL_CLASS': 'redis.connection.BlockingConnectionPool',
+    }
+}
+
 THEME = 'bootstrap'
 
