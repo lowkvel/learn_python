28dbaabd4a32d67a55bc9209da876b8fc62eb459
cy <35268618+lowkvel@users.noreply.github.com>
2021-01-18 14:36:40 +0800

    prod

--- typeidea/typeidea/typeidea/settings/prod.py
+++ typeidea/typeidea/typeidea/settings/prod.py
@@ -1,12 +1,20 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+ALLOWED_HOSTS = ['127.0.0.1']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'typeidea_db',
+        'USER': 'root',
+        'PASSWORD': 'root2333',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        #'CONN_MAX_AGE': 5*60,
+        #'OPTIONS': {'charset': 'utf8mb4'}
     }
 }
 
