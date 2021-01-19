eea5a2c50fba41926657a5cebfc1b04593d63751
cy <35268618+lowkvel@users.noreply.github.com>
2021-01-19 18:05:16 +0800

    simple json serving done

--- drestf_tutorial/tutorial/snippets/views.py
+++ drestf_tutorial/tutorial/snippets/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
 
-# Create your views here.
+# snippet_list, regular django views, v1
+@csrf_exempt
+def snippet_list(request):
+    if request.method == 'GET':                                 # list all code snippets
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    elif request.method == 'POST':                              # or create a new snippet
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+# snippet_detail, regular django views, v1
+@csrf_exempt
+def snippet_detail(request, pk):
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return HttpResponse(status=404)
+    
+    if request.method == 'GET':                                 # retrieve a specific snippet
+        serializer = SnippetSerializer(snippet)
+        return JsonResponse(serializer.data)
+    elif request.method == 'PUT':                               # update a specific snippet
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(snippet, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+    elif request.method == 'DELETE':                            # delete a specific snippet
+        snippet.delete()
+        return HttpResponse(status=204)
+
+
--- drestf_tutorial/tutorial/tutorial/urls.py
+++ drestf_tutorial/tutorial/tutorial/urls.py
@@ -23,6 +23,10 @@ router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+
+    # quickstart urls
+    #path('', include(router.urls)),
+    #path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
+    path('', include('snippets.urls'))
 ]
