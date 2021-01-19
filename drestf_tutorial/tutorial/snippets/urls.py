99bd1e92c363655ed64cbbc33c8ae4fdb7742eb1
cy <35268618+lowkvel@users.noreply.github.com>
2021-01-19 18:54:42 +0800

    views updated to drestf format and url suffix patterns

--- drestf_tutorial/tutorial/snippets/urls.py
+++ drestf_tutorial/tutorial/snippets/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
     path('snippets/', views.snippet_list),
     path('snippets/<int:pk>/', views.snippet_detail),
 ]
+
+# drestf url suffix patterns support, add 'format=None' in methods in views.py
+# such as: http://localhost:8000/snippets.json
+urlpatterns = format_suffix_patterns(urlpatterns) 
--- drestf_tutorial/tutorial/snippets/views.py
+++ drestf_tutorial/tutorial/snippets/views.py
@@ -1,9 +1,13 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+#from django.http import HttpResponse, JsonResponse
+#from django.views.decorators.csrf import csrf_exempt
+#from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
+"""
 # snippet_list, regular django views, v1
 @csrf_exempt
 def snippet_list(request):
@@ -18,7 +22,23 @@ def snippet_list(request):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+"""
 
+# snippet_list, drestf based views, v2
+@api_view(['GET', 'POST'])
+def snippet_list(request, format=None):
+    if request.method == 'GET':                                 # list all code snippets
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':                              # or create a new snippet
+        serializer = SnippetSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+"""
 # snippet_detail, regular django views, v1
 @csrf_exempt
 def snippet_detail(request, pk):
@@ -40,5 +60,25 @@ def snippet_detail(request, pk):
     elif request.method == 'DELETE':                            # delete a specific snippet
         snippet.delete()
         return HttpResponse(status=204)
+"""
 
-
+# snippet_detail, drestf based views, v2
+@api_view(['GET', 'PUT', 'DELETE'])
+def snippet_detail(request, pk, format=None):
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':                                 # retrieve a specific snippet
+        serializer = SnippetSerializer(snippet)
+        return Response(serializer.data)
+    elif request.method == 'PUT':                               # update a specific snippet
+        serializer = SnippetSerializer(snippet, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':                            # delete a specific snippet
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
