e166a2a95f489bd515afd46c311304763d6da263
cy <35268618+lowkvel@users.noreply.github.com>
2021-02-02 15:53:32 +0800

    router

--- drestf_tutorial/tutorial/snippets/urls.py
+++ drestf_tutorial/tutorial/snippets/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
 from rest_framework import renderers
 from snippets import views
 from snippets.views import SnippetViewSet, UserViewSet, api_root
 
+"""
 snippet_list = SnippetViewSet.as_view({
     'get': 'list',
     'post': 'create',
@@ -23,9 +25,15 @@ user_list = UserViewSet.as_view({
 user_detail = UserViewSet.as_view({
     'get': 'retrieve',
 })
+"""
 
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
+
+"""
 urlpatterns = [
-
+    
     path('', views.api_root),       # api-root for snippet app
 
     #path('snippets/', views.snippet_list, name='snippet-list'),             # used with snippet_list views v1 and v2
@@ -43,9 +51,14 @@ urlpatterns = [
     path('users/', user_list, name='user-list'),                 # used with user_list
     #path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),    # used with user_detail
     path('users/<int:pk>/', user_detail, name='user-detail'),    # used with user_detail
-
+    
 ]
 
 # drestf url suffix patterns support, add 'format=None' in views in views.py
 # such as: http://localhost:8000/snippets.json
 urlpatterns = format_suffix_patterns(urlpatterns) 
+"""
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
